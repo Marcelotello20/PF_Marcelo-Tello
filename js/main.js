eb76b3306a17a537e0cbbbbeb62a0de76b286189
Marcelo Tello <zhelobeats@gmail.com>
2023-12-04 19:08:53 -0300

    agregando formulario con js interactivo de bienvenida pagina anterior

--- js/main.js
+++ js/main.js
@@ -1,3 +1,56 @@
+//Formulario Index //
+
+// Clase para construir los datos cuando se ingresen
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad; 
+    }
+}
+
+let saludoIntro = "¡Hola! Es un gusto";
+let mayorDeEdad = "Eres mayor de edad y puedes disfrutar del contenido completo de nuestro sitio.";
+let menorDeEdad = "Al ser menor te recomendamos que para comprar tengas el consentimiento de tus padres o un adulto a cargo de ti.";
+let datoNoIntroducido = "Completar datos solicitados por favor.";
+
+function ingresarDatos() {
+    // Obtener el valor del input
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+
+    // Verificar si se introdujeron datos
+    if (nombre === "" || edad === "") {
+        document.getElementById("resultado").innerText = "Hola " + datoNoIntroducido;
+    } else {
+        // Crear un individuo de la clase Persona
+        let persona = new Persona(nombre, parseInt(edad, 10));
+
+        // Modificar el DOM para mostrar el saludo
+        if (persona.edad >= 18) {
+            document.getElementById("resultado").innerText = saludoIntro + " " + persona.nombre + "! " + mayorDeEdad;
+        } else {
+            document.getElementById("resultado").innerText = saludoIntro + " " + persona.nombre + "! " + menorDeEdad;
+        }
+
+        // Guardar el objeto Persona en el localStorage
+        localStorage.setItem("persona", JSON.stringify(persona));
+
+        // Actualizar el encabezado con el nombre introducido
+        document.getElementById("headerSaludo").innerHTML = `<h1>Bienvenido</h1><h2>${nombre}</h2>`;
+    }
+}
+
+//Obtener los datos de la Persona desde el localStorage
+const personaGuardada = localStorage.getItem("persona");
+if (personaGuardada) {
+    const persona = JSON.parse(personaGuardada);
+    
+}
+
+
+// PRODUCTOS Y CARRITO //
+
+
 const carrito = document.getElementById('carrito')
 const lista = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
@@ -34,6 +87,8 @@ cargarEventListeners();
 function cargarEventListeners() {
     elementos1.addEventListener('click', comprarElemento);
     elementos2.addEventListener('click', comprarElemento);
+    elementos3.addEventListener('click', comprarElemento);
+    elementos4.addEventListener('click', comprarElemento);
     carrito.addEventListener('click', eliminarElemento);
     vaciarCarritoBtn.addEventListener('click', vaciarCarrito); 
 }
@@ -101,3 +156,4 @@ function vaciarCarrito() {
     }
     return false;
 }
+
